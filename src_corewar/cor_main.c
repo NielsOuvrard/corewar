194a4ad27100008cd0d3083dce69df8a1b462d60
Niels Ouvrard <niels.ouvrard@epitech.eu>
2022-05-02 17:20:54 +0200

    file -> array (perfect vg ?)

--- src_corewar/cor_main.c
+++ src_corewar/cor_main.c
@@ -31,86 +31,7 @@ MEM_SIZE modulo.\n");
     return 0;
 }
 
-// register = 1 value
-
-// APC = compteur de programme
-// contient ptr vers prochaine execution
-// pour savoir où on est / ecrir choses dans la mémoire
-
-// drapeau carry = faux si dernière opération vaut 0
-
-
-// VM : On envoie executions en param, elle execute
-
-// cycle CYCLE_TO_DIE verif que instruction life est emise ? vie : meurt
-
-// si après NBR_LIVE executions de "live" , process tjrs en vie, CYCLE_TO_DIE -= CYCLE_DELTA unitées
-// -> jusqu'a ce qu'in n'y ait plus de process "live"
-
-
-// registre r1 = numéro des programmes donnés au démarrage
-
-
-// chaque element s'executes à la fin de son dernier cycle
-
-int ascii_to_hex(char c)
-{
-    int num = (int) c;
-    if (num < 58 && num > 47)
-        return num - 48;
-    if (num < 103 && num > 96)
-        return num - 87;
-    return num;
-}
-
-// int main()
-// {
-//         FILE *fp = fopen("sample","r");
-//         unsigned char c1,c2;
-//         int i = 0;
-//         unsigned char sum,final_hex[15/2];
-//         for(i = 0; i < 15 / 2; i++)
-//         {
-//             c1 = ascii_to_hex(fgetc(fp));
-//             c2 = ascii_to_hex(fgetc(fp));
-//             sum = c1 << 4 | c2;
-//             final_hex[i] = sum;
-//             printf("%02x ", sum);
-//         }
-//         printf("\n");
-// }
-
-char *char_to_bin_str (char c)
-{
-    char *str = malloc(sizeof(char) * 9);
-    for (int i = 0; i < 8; i++)
-        str[i] = (!!((c << i) & 0x80)) + '0';
-    str[8] = '\0';
-    return str;
-}
-
-char *my_scanf (void)
-{
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t lower_case = 0;
-    lower_case = getline(&line, &len, stdin);
-    if (lower_case < 0) {
-        free(line);
-        return NULL;
-    }
-    return line;
-}
-
-int my_base_to_int(char *src, int base)
-{
-    int result = 0, b = 0;
-    int i = my_strlen(src);
-    for (i--; i >= 0; i--) {
-        result += CTOI(src[i]) * my_compute_power_rec(base, b++);
-    }
-    return result;
-}
+char **file_cor_to_array(char *filepath);
 
 int main (int ac, char **av)
 {
@@ -118,17 +39,12 @@ int main (int ac, char **av)
         return help();
     if (ac < 2)
         return 1;
-    my_printf("on ouvre %s\n", av[1]);
-    char *str = filepath_to_str(av[1]);
-    for (int i = 0; i < MEM_SIZE; i++) {
-        if (str[i]) {
-            char *binary = char_to_bin_str(str[i]);
-            char *hexa = my_int_to_base(my_base_to_int(binary, 2), 16);
-            my_printf("%s ", hexa);
-            free(binary);
-            char *base = my_int_to_base(my_abs((int)str[i]), 16);
-        }
-    }
+    char **file = file_cor_to_array(av[1]);
+    if (!file)
+        return 84;
+    my_printf("\nle array :\n");
+    my_show_word_array(file);
+    free_my_arr(file);
     // my_putstr("The player NB_OF_PLAYER(NAME_OF_PLAYER)is alive.\n");
     // my_putstr("The player NB_OF_PLAYER(NAME_OF_PLAYER)has won.");
     // my_printf("REG_NUMBER : %d\n", REG_NUMBER);
---
+++ src_corewar/cor_open_file_cor.c
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2022
+** rpg
+** File description:
+** pour open file cor
+*/
+
+#include "op.h"
+#include "my.h"
+#include "cor_header.h"
+
+char *char_to_bin_str (char c)
+{
+    char *str = malloc(sizeof(char) * 9);
+    for (int i = 0; i < 8; i++)
+        str[i] = (!!((c << i) & 0x80)) + '0';
+    str[8] = '\0';
+    return str;
+}
+
+int my_base_to_int(char *src, int base)
+{
+    int result = 0, b = 0, i = my_strlen(src);
+    for (i--; i >= 0; i--)
+        result += CTOI(src[i]) * my_compute_power_rec(base, b++);
+    return result;
+}
+
+char **copy_array_plus_one (char **src, char *one)
+{
+    int size = my_arraylen(src), i;
+    char **dest = malloc(sizeof(char *) * (size + 2));
+    if (dest == NULL)
+        return NULL;
+    for (i = 0; src[i] != NULL; i++)
+        dest[i] = src[i];
+    dest[i] = one;
+    dest[i + 1] = NULL;
+    free(src);
+    return dest;
+}
+
+char **file_cor_to_array(char *filepath)
+{
+    char *str = filepath_to_str(filepath);
+    if (!str)
+        return NULL;
+    char **array = malloc(sizeof(char *));
+    array[0] = NULL;
+    for (int i = 0; i < MEM_SIZE; i++) {
+        if (str[i]) {
+            char *binary = char_to_bin_str(str[i]);
+            char *hexa = my_int_to_base(my_base_to_int(binary, 2), 16);
+            free(binary);
+            array = copy_array_plus_one(array, hexa);
+        }
+    }
+    return array;
+}
