456457125a4cf9a629320581ee7f3a95fb38176a
Niels Ouvrard <niels.ouvrard@epitech.eu>
2022-05-05 17:48:50 +0200

    on lit bien, sauf fonction exceptiosn

--- src_corewar/cor_virtual_machine.c
+++ src_corewar/cor_virtual_machine.c
@@ -12,6 +12,17 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
+// brk
+// sbrk
+
+// strace
+
+// struct a {
+//     unsigned var : 7;
+//     unsigned vari : 32;
+    // int vari;
+// };
+
 int modify_bit_n_in_int (int nmb, int where, bool bit)
 {
     if (bit)
@@ -40,10 +51,10 @@ void print_int_bits (unsigned int num){
 unsigned int char_nmb_to_int (unsigned char *str, int size)
 {
     unsigned int elem = 0;
-    int index_str = 0;
-    for (int i = 0; i < 8 * size; i++) {
+    int index_str = size - 1;
+    for (int i = 0; i < (8 * size); i++) {
         if (!(i % 8) && i > 0)
-            index_str++;
+            index_str--;
         if (str[index_str] & 1)
             elem = modify_bit_n_in_int(elem, i, 1);
         str[index_str] >>= 1;
@@ -69,9 +80,14 @@ unsigned int char_nmb_to_int (unsigned char *str, int size)
 
 void life (unsigned char *str, int index)
 {
-    index += 1;
+    // my_putchar('\n');
+    // for (int i = 0; i < 4; i++) {
+    //     print_char_bits(str[index + i]);
+    //     my_putchar(' ');
+    // }
     int nmb_player = char_nmb_to_int(str + index, 4);
-    my_printf("The player %d(NAME_OF_PLAYER)is alive.\n", nmb_player);
+    // print_int_bits(nmb_player);
+    my_printf("\nThe player %d(NAME_OF_PLAYER)is alive.\n", nmb_player);
 }
 
 char return_type_char (char *type)
@@ -102,50 +118,71 @@ char *type_param_to_str (unsigned char param)
     return all_types;
 }
 
-char *recup_params_according_to_str (unsigned char *str, char *params)
+int recup_params_according_to_str (unsigned char *str, char *params, int *arr_args, int nmb_funct)
 {
-    int index = 0;
+    int index = 0, index_arr = 1;
     for (int i = 0; params[i]; i++) {
+        my_printf("\non trouve arg %c -> %d\n", params[i], (unsigned int)str[index]);
+        my_printf("index : %d\n", index);
         if (params[i] == 'r') {
-            // unsigned char param = str[index];
+            arr_args[index_arr++] = (int)str[index];
             index += 1;
         }
-        if (params[i] == 'd') {
-            // unsigned char param = str[index];
+        if (params[i] == 'd' && nmb_funct != 10 && nmb_funct != 11) {
+            arr_args[index_arr++] = char_nmb_to_int(str + index, 4);    // juste pour index, mettre 4
             index += 4;
-            for (int i = 0; i < 4; i++) {
-                print_char_bits(str[index + i]);
-                my_printf("\n");
-            }
-            print_int_bits(char_four_to_int(str + index));
         }
-        if (params[i] == 'i') {
-            // unsigned char param = str[index];
+        if (params[i] == 'i' || (params[i] == 'd' && (nmb_funct == 10 || nmb_funct == 11))) {
+            arr_args[index_arr++] = char_nmb_to_int(str + index, 2);
             index += 2;
         }
     }
-    return 0;
+    return index;
+}
+
+int how_many_args (char *list_args)
+{
+    int size = 0;
+    for (int i = 0; list_args[i]; i++) {
+        if (list_args[i] != ' ')
+            size++;
+    }
+    return size;
 }
 
 void check_all_functions (unsigned char *str, int size)
 {
     int index;
     for (index = 0; str[index] > 0x10 || str[index] < 1 && index < size; index++);
-    for (; index < size; index++) {
+    while (index < size) {
         int nmb_funct = str[index] -  1;
         op_t val = op_tab[nmb_funct];
         my_printf("\nfirst command : `%s` car %d\n", val.mnemonique, str[index]);
         index++;
         if (nmb_funct == 0) {
-            life(str, index + 1);
-            return;
+            life(str, index);
+            index += 4;
+            my_printf("index %d ? size %d\n", index, size);
+            // return;
+        } else {
+            char *params_types_str = type_param_to_str(str[index]);
+            int nmb_args = how_many_args(params_types_str);
+            int *args = malloc(sizeof(int) * (nmb_args + 1));
+            args[0] = nmb_args;
+            char parameter_type = str[index];
+            if (!parameter_type)
+                return;
+            my_printf("on a à la fin `%s` car %d\n", params_types_str, str[index]);
+            index++;
+            index += recup_params_according_to_str(str + index, params_types_str, args, nmb_funct);
+            my_printf("nmb args : %d\n", args[0]);
+            for (int index_arr = 1; index_arr <= args[0]; index_arr++) {
+                my_printf("arg %d = %d\n", index_arr, args[index_arr]);
+            }
+            my_printf("END\n");
+            free(args);
         }
-        char *params = type_param_to_str(str[index]);
-        char parameter_type = str[index];
-        if (!parameter_type)
-            return;
-        my_printf("on a à la fin `%s` car %d\n", params, str[index]);
-        recup_params_according_to_str(str + (++index), params);
+        // return;
         // struct params_s param;
         // my_printf("parameter type :", val.mnemonique, index);
         // x |= (1u << 3); // 4e bit = 1
@@ -158,7 +195,6 @@ void check_all_functions (unsigned char *str, int size)
         // }
         // types[8] = '\0';
         // my_printf("parameter type : %s\n", types);
-        return;
     }
 }
 
@@ -170,6 +206,7 @@ int virtual_machine (char *filepath)
         return 84;
     disp_str_to_hexa(str, size);
     check_all_functions(str, size);
+    my_printf("quit VM\n");
     // char *shorter = str_but_shorter(str, size);
     free(str);
     return 0;
